c14fae1c45565a801de4ae8cdc7abb3fbb38145d
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-09-07 13:32:20 -0400

    projects and skills

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,3 +1,8 @@
+import logo1 from "../assets/decryptor.png"
+import logo2 from "../assets/codedoctor.png"
+import logo3 from "../assets/neurolog.png"
+import logo4 from "../assets/finview.png"
+import './MainContainer.css'
 
 export default function MainContainer() {
   return (
@@ -8,39 +13,47 @@ export default function MainContainer() {
       </div>
       <div className='projects'>
         <h1>Projects</h1>
-        <div>
-          <div>
+        <div className='project-div'>
+          <div id='proj1'>
             <h2>DeCryptor</h2>
+            <img id='logo1' src={logo1} alt="" />
             <p>DeCryptor is an online community where crypto traders and enthusiasts can engage in a discussion and share their thoughts on the the most popular cryptocurrencies. Users can create an account, view information on top cryptocurrencies, post about a specific cryptocurrency, and comment on other posts.</p>
           </div>
-          <div>
+          <div id='proj2'>
             <h2>Code Doctor</h2>
+            <img id='logo2' src={logo2} alt="" />
             <p>Code Doctor is a forum for all things coding/programming/development. Users can create accounts, create posts with their coding questions, and reply to other users’ posts with comments/answers. The goal is to empower a community of developers of all levels to share knowledge and receive feedback.</p>
           </div>
-          <div>
+          <div id='proj3'>
             <h2>NeuroLog</h2>
+            <img id='logo3' src={logo3} alt="" />
             <p>NeuroLog is a react build which shows users a database of neuroscience-related research articles. Users will be able to view current research, add new articles, and delete them. It will have a second Airtable with a popular news section geared towards less science-inclined users.</p>
           </div>
-          <div>
+          <div id='proj4'>
             <h2>FinView</h2>
+            <img id='logo4' src={logo4} alt="" />
             <p>FinView offers a search by stock ticker. Real-time share price as well as a company profile with a financial overview for the searched stock will be provided.</p>
           </div>
         </div>
       </div>
       <div className='skills'>
         <h1>Skills</h1>
-        <div>
-          <div>
-            <h2>Languages and Frameworks</h2>
-            <p>JavaScript, React, Ruby,  Rails, Python, jQuery, Node, Express, AJAX, HTML, CSS, SQL, PostgreSQL, MongoDB.</p>
-          </div>
-          <div>
+        <div className='skills-div'>
+          <div id='management'>
             <h2>Management and Deployment</h2>
             <p>Git, GitHub, Command Line, RSpec, Heroku.</p>
           </div>
-          <div>
+          <div id='languages'>
+            <h2>Languages and Frameworks</h2>
+            <p>JavaScript, React, Ruby,  Rails, Python, jQuery, Node, Express, AJAX, HTML, CSS.</p>
+          </div>
+          <div id='methods'>
             <h2>Methodologies</h2>
             <p>Object Oriented Programming, MVC Pattern, Test-Driven Development, Responsive Design, Authentication, User Stories,  ERDs, Wireframing, Agile Development/Scrum.</p>
+          </div>
+          <div id='databases'>
+            <h2>Databases</h2>
+            <p>SQL, PostgreSQL, MongoDB.</p>
           </div>
         </div>
       </div>
