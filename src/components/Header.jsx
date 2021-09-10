73b6395ea004fff489a81d4946181e392d35d009
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-09-10 12:45:14 -0400

    css edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ export default function Header() {
         <h1 id='name'>Jared Beneroff</h1>
       </div>
       <div className='nav'>
-        <a className='nav-item' id='about-nav' href='#about-header'>About</a>
-        <a className='nav-item' id='projects-nav' href='#projects-header'>Projects</a>
-        <a className='nav-item' id='skills-nav' href='#skills-header'>Skills</a>
-        <a className='nav-item' id='contanct-nav' href='#contact-header'>Contact</a>
+        <a className='nav-item' id='about-nav' href='#about-link'>About</a>
+        <a className='nav-item' id='projects-nav' href='#projects-link'>Projects</a>
+        <a className='nav-item' id='skills-nav' href='#skills-link'>Skills</a>
+        <a className='nav-item' id='contanct-nav' href='#contact-link'>Contact</a>
       </div>
     </div>
   )
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -2,20 +2,22 @@ import logo1 from "../assets/decryptor.png"
 import logo2 from "../assets/codedoctor.png"
 import logo3 from "../assets/neurolog.png"
 import logo4 from "../assets/finview.png"
+import resume from "../assets/resume.pdf"
 import './MainContainer.css'
 
 export default function MainContainer() {
   return (
     <div>
-      <div className='about'>
+      <div className='about' id='about-link'>
         <h1 id='about-header'>About Me</h1>
         <p id='about-info'>I am a software engineer with an extensive and diverse background in full stack development, data analysis with python, and five years of preclinical neuroscience research looking to work in and advance a field utilizing new technologies.</p>
+        <a id='resume' href={resume} >View My Resume Here</a>
       </div>
-      <div className='projects'>
+      <div className='projects' id='projects-link'>
         <h1 id='projects-header'>Projects</h1>
         <div className='project-div'>
           <div id='proj1'>
-            <h2>DeCryptor</h2>
+            <h2 className='project-name' >DeCryptor</h2>
             <a href="https://decryptor.netlify.app/">
               <img id='logo1' src={logo1} alt="" />
             </a>
@@ -23,7 +25,7 @@ export default function MainContainer() {
             <p className='project-stack'>Ruby on Rails, React JS</p>
           </div>
           <div id='proj2'>
-            <h2>Code Doctor</h2>
+            <h2 className='project-name' >Code Doctor</h2>
             <a href="https://codedoctor.netlify.app/">
               <img id='logo2' src={logo2} alt="" />
             </a>
@@ -31,7 +33,7 @@ export default function MainContainer() {
             <p className='project-stack'>MongoDB, Express, React JS</p>
           </div>
           <div id='proj3'>
-            <h2>NeuroLog</h2>
+            <h2 className='project-name' >NeuroLog</h2>
             <a href="https://neurolog.netlify.app/">
               <img id='logo3' src={logo3} alt="" />
             </a>
@@ -39,7 +41,7 @@ export default function MainContainer() {
             <p className='project-stack'>React JS, AirTable</p>
           </div>
           <div id='proj4'>
-            <h2>FinView</h2>
+            <h2 className='project-name' >FinView</h2>
             <a href="https://jbeneroff.github.io/Stock-Data-Project/">
               <img id='logo4' src={logo4} alt="" />
             </a>
@@ -48,7 +50,7 @@ export default function MainContainer() {
           </div>
         </div>
       </div>
-      <div className='skills'>
+      <div className='skills' id='skills-link'>
         <h1 id='skills-header'>Skills</h1>
         <div className='skills-div'>
           <div id='management'>
@@ -61,7 +63,7 @@ export default function MainContainer() {
           </div>
           <div id='methods'>
             <h2>Methodologies</h2>
-            <p>Object Oriented Programming, MVC Pattern, Test-Driven Development, Responsive Design, Authentication, User Stories,  ERDs, Wireframing, Agile Development/Scrum, Troubleshooting/Debugging.</p>
+            <p>Object Oriented Programming, MVC, Test-Driven Development, Responsive Design, Authentication, Wireframing, Agile/Scrum, Troubleshooting/Debugging.</p>
           </div>
           <div id='databases'>
             <h2>Databases</h2>
@@ -69,12 +71,12 @@ export default function MainContainer() {
           </div>
         </div>
       </div>
-      <div className='contact'>
+      <div className='contact' id='contact-link'>
         <h1 id='contact-header'>Contact Me</h1>
         <p id='number'>(201) 572-5227</p>
         <p id='email'>jared.beneroff@gmail.com</p>
-        <a id='github' href="https://github.com/jbeneroff">GitHub</a>
         <a id='linkedin' href="https://www.linkedin.com/in/jared-beneroff/">LinkedIn</a>
+        <a id='github' href="https://github.com/jbeneroff">GitHub</a>
       </div>
     </div>
   )
